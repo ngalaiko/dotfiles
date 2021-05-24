fdc776a8e1a712c4cc0ce864503babd173957d28
Nikita Galaiko <nikita@galaiko.rocks>
2021-05-24 21:30:58 +0200

    vim: set correct autopairs mapping

--- .config/nvim/vimrc/plugins/lua/nvim-autopairs.lua
+++ .config/nvim/vimrc/plugins/lua/nvim-autopairs.lua
@@ -1,14 +1,19 @@
-local npairs = require'nvim-autopairs'
+local remap = vim.api.nvim_set_keymap
+local npairs = require('nvim-autopairs')
 
-npairs.setup()
+_G.MUtils= {}
 
-function _G.completions()
-    if vim.fn.pumvisible() == 1 then
-        if vim.fn.complete_info()["selected"] ~= -1 then
-            return vim.fn["compe#confirm"]("<CR>")
-        end
+vim.g.completion_confirm_key = ""
+MUtils.completion_confirm=function()
+  if vim.fn.pumvisible() ~= 0  then
+    if vim.fn.complete_info()["selected"] ~= -1 then
+      return vim.fn["compe#confirm"](npairs.esc("<cr>"))
+    else
+      return npairs.esc("<cr>")
     end
-    return npairs.check_break_line_char()
+  else
+    return npairs.autopairs_cr()
+  end
 end
 
-vim.api.nvim_set_keymap("i", "<CR>", "v:lua.completions()", {expr = true})
+remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
