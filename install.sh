8e0b28356c136cecd6c630623641460b791544a6
Nikita Galaiko <nikita@galaiko.rocks>
2021-08-12 08:54:05 +0200

    zsh: move to subdir

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ ln -sf $(pwd)/.zsh_plugins.txt "$HOME/"
 ln -sf $(pwd)/.gitconfig "$HOME/"
 ln -sf $(pwd)/.config/nvim "$HOME/.config/"
 ln -sf $(pwd)/.config/kitty "$HOME/.config/"
+ln -sf $(pwd)/.config/zsh "$HOME/.config/"
 ln -sf $(pwd)/.config/starship.toml "$HOME/.config/"
 
 brew bundle install
