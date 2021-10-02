d774ea661c3e1b12824dde8592c774e6162ef788
Nikita Galaiko <nikita@galaiko.rocks>
2021-10-02 19:08:24 +0200

    nvim: more nvim-tree cfg

--- .config/nvim/vimrc/plugins/lua/nvim-tree.lua
+++ .config/nvim/vimrc/plugins/lua/nvim-tree.lua
@@ -2,4 +2,12 @@ require'nvim-tree'.setup{
     open_on_setup       = true,
     auto_close          = true,
     update_cwd          = true,
+    hijack_cursor       = true,
+    update_focused_file = {
+        enable      = true,
+        update_cwd  = false,
+    },
+    view = {
+        auto_resize = true,
+    }
 }
