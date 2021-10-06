da16a4e8226492db42afbd7ba9595e35631ee337
Nikita Galaiko <nikita@galaiko.rocks>
2021-10-06 16:21:45 +0200

    nvim: do not focus nvim-tree

--- .config/nvim/vimrc/plugins/lua/nvim-tree.lua
+++ .config/nvim/vimrc/plugins/lua/nvim-tree.lua
@@ -1,12 +1,7 @@
 require'nvim-tree'.setup{
     open_on_setup       = true,
     auto_close          = true,
-    update_cwd          = true,
     hijack_cursor       = true,
-    update_focused_file = {
-        enable      = true,
-        update_cwd  = false,
-    },
     view = {
         auto_resize = true,
     }
