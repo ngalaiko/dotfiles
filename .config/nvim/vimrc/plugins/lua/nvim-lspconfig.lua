504801f2fab806de286825a7c52047a89d807304
Nikita Galaiko <nikita@galaiko.rocks>
2021-11-18 08:44:14 +0100

    nvim: add lsp icons

--- .config/nvim/vimrc/plugins/lua/nvim-lspconfig.lua
+++ .config/nvim/vimrc/plugins/lua/nvim-lspconfig.lua
@@ -25,6 +25,20 @@ local servers = {
     svelte = {},
 }
 
+-- prefix/character preceding the diagnostics' virtual text
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = {
+    prefix = '●', -- Could be '■', '▎', 'x'
+  }
+})
+
+-- replace the default lsp diagnostic letters with prettier symbols
+local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 -- nvim-cmp source setup
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
@@ -33,8 +47,10 @@ for server, value in pairs(servers) do
     nvim_lsp[server].setup(value)
 end
 
--- replace the default lsp diagnostic letters with prettier symbols
-vim.fn.sign_define("LspDiagnosticsSignError", {text = "", numhl = "LspDiagnosticsDefaultError"})
-vim.fn.sign_define("LspDiagnosticsSignWarning", {text = "", numhl = "LspDiagnosticsDefaultWarning"})
-vim.fn.sign_define("LspDiagnosticsSignInformation", {text = "", numhl = "LspDiagnosticsDefaultInformation"})
-vim.fn.sign_define("LspDiagnosticsSignHint", {text = "", numhl = "LspDiagnosticsDefaultHint"})
+-- change diagnostic signs
+vim.cmd [[
+  sign define DiagnosticSignError text=  texthl=LspDiagnosticsSignError linehl= numhl=LspDiagnosticsLineNrError
+  sign define DiagnosticSignWarn text=  texthl=LspDiagnosticsSignWarning linehl= numhl=LspDiagnosticsLineNrWarning
+  sign define DiagnosticSignInfo text=  texthl=LspDiagnosticsSignInformation linehl= numhl=LspDiagnosticsLineNrInformation
+  sign define DiagnosticSignHint text=  texthl=LspDiagnosticsSignHint linehl= numhl=LspDiagnosticsLineNrHint
+]]
