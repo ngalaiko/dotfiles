702de69dd19f75e3cdfc851dc2b5cc636c22c458
Nikita Galaiko <nikita@galaiko.rocks>
2021-11-29 15:58:06 +0100

    nvim: telescope: do not search js crap

--- .config/nvim/vimrc/plugins/lua/telescope.lua
+++ .config/nvim/vimrc/plugins/lua/telescope.lua
@@ -1,16 +1,16 @@
-local telescope = require'telescope'
+local telescope = require "telescope"
 
-telescope.setup{
+telescope.setup {
   defaults = {
     vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-      '--hidden'
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden"
     },
     prompt_prefix = "> ",
     selection_caret = "> ",
@@ -21,28 +21,31 @@ telescope.setup{
     layout_strategy = "vertical",
     layout_config = {
       horizontal = {
-        mirror = false,
+        mirror = false
       },
       vertical = {
         width = 0.8,
-        mirror = false,
-      },
+        mirror = false
+      }
     },
-    file_sorter =  require'telescope.sorters'.get_fuzzy_file,
-    file_ignore_patterns = {},
-    generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+    file_sorter = require "telescope.sorters".get_fuzzy_file,
+    file_ignore_patterns = {
+      "node_modules",
+      "dist",
+      "__generated__"
+    },
+    generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
     winblend = 0,
     border = {},
-    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+    borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
     color_devicons = true,
     use_less = true,
     path_display = {},
-    set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-
+    set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
+    file_previewer = require "telescope.previewers".vim_buffer_cat.new,
+    grep_previewer = require "telescope.previewers".vim_buffer_vimgrep.new,
+    qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
     -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+    buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker
   }
 }
