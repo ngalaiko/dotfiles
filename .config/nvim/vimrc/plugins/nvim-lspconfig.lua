8140f2efc63a2e19c3b61ca39ac766b70f90efcf
Nikita Galaiko <nikita@galaiko.rocks>
2022-11-08 14:04:41 +0100

    nvim: update config

--- .config/nvim/vimrc/plugins/nvim-lspconfig.lua
+++ .config/nvim/vimrc/plugins/nvim-lspconfig.lua
@@ -66,7 +66,7 @@ for type, icon in pairs(signs) do
 end
 
 -- nvim-cmp source setup
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 for server, value in pairs(servers) do
 	value.capabilities = capabilities
 end
