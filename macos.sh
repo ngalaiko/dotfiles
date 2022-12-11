40aaf8c1a2dbf92f964e26be40a660a13a70c9c6
Nikita Galaiko <nikita@galaiko.rocks>
2022-12-11 17:41:41 +0100

    macos: add options

--- macos.sh
+++ macos.sh
@@ -2,3 +2,7 @@
 
 # disable font smoothing
 defaults -currentHost write -g AppleFontSmoothing -int 0
+defaults write -g CGFontRenderingFontSmoothingDisabled -bool true
+
+# show mail attachments as icons
+defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
