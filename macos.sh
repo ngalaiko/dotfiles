8d89303fe89c63f07a9d68e92e9a708af83d81fa
Nikita Galaiko <nikita@galaiko.rocks>
2024-01-08 11:51:58 +0100

    macos: disable sonoma language switch bubble

--- macos.sh
+++ macos.sh
@@ -6,3 +6,6 @@ defaults write -g CGFontRenderingFontSmoothingDisabled -bool true
 
 # show mail attachments as icons
 defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
+
+# ddisable sonoma language switch bubble
+defaults write /Library/Preferences/FeatureFlags/Domain/UIKit.plist redesigned_text_cursor -dict-add Enabled -bool NO
