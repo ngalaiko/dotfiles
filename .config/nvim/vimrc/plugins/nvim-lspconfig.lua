b268ca32f5152f6407ac1f6f2a91106853041c68
Nikita Galaiko <nikita@galaiko.rocks>
2024-04-10 16:53:42 +0200

    nvim: enable inline hints

--- .config/nvim/vimrc/plugins/nvim-lspconfig.lua
+++ .config/nvim/vimrc/plugins/nvim-lspconfig.lua
@@ -89,3 +89,15 @@ vim.cmd([[
 for server, value in pairs(servers) do
 	nvim_lsp[server].setup(value)
 end
+
+-- enable inline hints
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(args.buf, true)
+        end
+        -- whatever other lsp config you want
+    end
+})
