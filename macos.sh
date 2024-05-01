b5fce20064ac59cb7af4bba8d9d265a64dbb8d05
Nikita Galaiko <nikita@galaiko.rocks>
2024-05-01 10:56:55 +0200

    macos: do not launch music on play

--- macos.sh
+++ macos.sh
@@ -7,5 +7,8 @@ defaults write -g CGFontRenderingFontSmoothingDisabled -bool true
 # show mail attachments as icons
 defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
 
-# ddisable sonoma language switch bubble
+# disable sonoma language switch bubble
 defaults write /Library/Preferences/FeatureFlags/Domain/UIKit.plist redesigned_text_cursor -dict-add Enabled -bool NO
+
+# do not launch Music.app when play is pressed
+launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist
