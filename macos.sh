1af4f0957289dd6855e56233feef1f5c394d4a92
Nikita Galaiko <nikita@galaiko.rocks>
2024-07-02 10:07:31 +0200

    macos: more stuff

--- macos.sh
+++ macos.sh
@@ -12,3 +12,24 @@ defaults write /Library/Preferences/FeatureFlags/Domain/UIKit.plist redesigned_t
 
 # do not launch Music.app when play is pressed
 launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Set the icon size of Dock items to 36 pixels
+defaults write com.apple.dock tilesize -int 36
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Restart the Dock
+killall Dock
+
+# Restart finder
+killall Finder
+
+# Restart SystemUIServer
+killall SystemUIServer
