76c7e9be39e37848fd8cc41aa50b14edc93148fd
Nikita Galaiko <nikita@galaiko.rocks>
2024-07-10 16:13:05 +0200

    nvim: format react

--- .config/nvim/vimrc/plugins/conform.lua
+++ .config/nvim/vimrc/plugins/conform.lua
@@ -2,7 +2,9 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		javascript = { { "prettierd", "prettier" } },
+		javascriptreact = { { "prettierd", "prettier" } },
 		typescript = { { "prettierd", "prettier" } },
+		typescriptreact = { { "prettierd", "prettier" } },
 		go = { "gofumpt", "gci", "goimports" },
 		python = { "black" },
 		terraform_fmt = { "tf", "tfvars", "hcl", "terraform" },
