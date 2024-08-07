002cb8dc0bd9d9960e5d6c92576d328676678ede
Nikita Galaiko <nikita@galaiko.rocks>
2024-08-07 12:27:55 +0200

    nvim: fix cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,17 +1,63 @@
 return {
 	"https://github.com/hrsh7th/nvim-cmp",
-	depencencies = {
-		"https://github.com/hrsh7th/cmp-nvim-lsp",
-		"https://github.com/hrsh7th/cmp-buffer",
-		"https://github.com/hrsh7th/cmp-path",
-		"https://github.com/hrsh7th/cmp-cmdline",
+	dependencies = {
+		{ "https://github.com/hrsh7th/cmp-buffer" },
+		{ "https://github.com/hrsh7th/cmp-path" },
+		{
+			"https://github.com/hrsh7th/cmp-nvim-lsp",
+			dependencies = {
+				{
+					"https://github.com/L3MON4D3/LuaSnip",
+					dependencies = {
+						"https://github.com/saadparwaiz1/cmp_luasnip",
+						"https://github.com/rafamadriz/friendly-snippets",
+					},
+					keys = {
+						{
+							"<tab>",
+							function()
+								return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+							end,
+							expr = true,
+							silent = true,
+							mode = "i",
+						},
+						{
+							"<tab>",
+							function()
+								require("luasnip").jump(1)
+							end,
+							mode = "s",
+						},
+						{
+							"<s-tab>",
+							function()
+								require("luasnip").jump(-1)
+							end,
+							mode = { "i", "s" },
+						},
+					},
+				},
+			},
+		},
+		{ "https://github.com/hrsh7th/cmp-cmdline" },
 	},
-	config = function()
+	config = function(_, opts)
 		local cmp = require("cmp")
+		require("luasnip.loaders.from_vscode").lazy_load()
 
 		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
 			view = {
 				entries = { name = "custom", selection_order = "near_cursor" },
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
 			},
 			mapping = {
 				["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
@@ -30,26 +76,39 @@ return {
 					i = cmp.mapping.select_next_item(),
 				},
 			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
-				{ name = "nvim_lua" },
-				{ name = "conjure" },
-				{ name = "buffer" },
 				{ name = "path" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+			}),
+		})
+
+		-- `/` cmdline setup.
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+
+		-- `:` cmdline setup.
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{
+					name = "cmdline",
+					option = {
+						ignore_cmds = { "Man", "!" },
+					},
+				},
 			}),
 		})
 
 		-- autopairs setup
 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
-
-		vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
-		-- Disable omni completion for SQL
-		vim.g.omni_sql_default_compl_type = "syntax"
 	end,
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
 	"https://github.com/nvim-telescope/telescope.nvim",
 	version = "0.1.8",
-	depencencies = { "https://github.com/nvim-lua/plenary.nvim" },
+	dependencies = { "https://github.com/nvim-lua/plenary.nvim" },
 	config = function()
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
