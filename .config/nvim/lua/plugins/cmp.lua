f2c7f3860e5d2db311403e0409fb50a71eac1d1c
Nikita Galaiko <nikita@galaiko.rocks>
2024-08-07 15:35:01 +0200

    nvim: rm snippet

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -3,55 +3,12 @@ return {
 	dependencies = {
 		{ "https://github.com/hrsh7th/cmp-buffer" },
 		{ "https://github.com/hrsh7th/cmp-path" },
-		{
-			"https://github.com/hrsh7th/cmp-nvim-lsp",
-			dependencies = {
-				{
-					"https://github.com/L3MON4D3/LuaSnip",
-					dependencies = {
-						"https://github.com/saadparwaiz1/cmp_luasnip",
-						"https://github.com/rafamadriz/friendly-snippets",
-					},
-					keys = {
-						{
-							"<tab>",
-							function()
-								return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-							end,
-							expr = true,
-							silent = true,
-							mode = "i",
-						},
-						{
-							"<tab>",
-							function()
-								require("luasnip").jump(1)
-							end,
-							mode = "s",
-						},
-						{
-							"<s-tab>",
-							function()
-								require("luasnip").jump(-1)
-							end,
-							mode = { "i", "s" },
-						},
-					},
-				},
-			},
-		},
+		{ "https://github.com/hrsh7th/cmp-nvim-lsp" },
 		{ "https://github.com/hrsh7th/cmp-cmdline" },
 	},
 	config = function(_, opts)
 		local cmp = require("cmp")
-		require("luasnip.loaders.from_vscode").lazy_load()
-
 		cmp.setup({
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
-				end,
-			},
 			view = {
 				entries = { name = "custom", selection_order = "near_cursor" },
 			},
@@ -79,7 +36,6 @@ return {
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
 				{ name = "path" },
-				{ name = "luasnip" },
 				{ name = "buffer" },
 			}),
 		})
