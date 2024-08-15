eec6375051c430264acd9bef2d6253bdd90442a8
Nikita Galaiko <nikita@galaiko.rocks>
2024-08-15 10:30:07 +0200

    nvim: telescope find files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,18 @@
 return {
 	"https://github.com/nvim-telescope/telescope.nvim",
 	version = "0.1.8",
+	opts = {
+		pickers = {
+			find_files = {
+				find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "--glob", "!**/node_modules/*" },
+			},
+		},
+	},
 	dependencies = { "https://github.com/nvim-lua/plenary.nvim" },
-	config = function()
+	config = function(_, opts)
+		require("telescope").setup(opts)
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-		vim.keymap.set("n", "<C-P>", builtin.git_files, {})
+		vim.keymap.set("n", "<C-P>", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>ps", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end)
