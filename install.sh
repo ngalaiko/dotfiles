f92923bfab310be33ef5d5beca069b558c26394d
Nikita Galaiko <nikita@galaiko.rocks>
2024-12-05 14:10:06 +0100

    zed: configure

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ ln -sf $(pwd)/.config/git "$HOME/.config/"
 ln -sf $(pwd)/.config/wezterm "$HOME/.config/"
 ln -sf $(pwd)/.config/starship.toml "$HOME/.config/starship.toml"
 ln -sf $(pwd)/.config/ghostty "$HOME/.config/"
+ln -sf $(pwd)/.config/zed "$HOME/.config/"
 
 # gpg
 mkdir -p "$HOME/.gnupg"
