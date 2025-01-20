3ee32d6dfd1a0dd0f6e1bec0ef6b18ed7826c2bc
Nikita Galaiko <nikita@galaiko.rocks>
2025-01-20 07:12:33 +0100

    fish: set color scheme

--- .config/fish/conf.d/fish.fish
+++ .config/fish/conf.d/fish.fish
@@ -1,2 +1,41 @@
 set -U fish_greeting         # disable fish greeting
 set -U fish_color_valid_path # do not underline valid paths
+
+# Colorscheme: Mono Smoke
+set -U fish_color_normal normal
+set -U fish_color_command ffffff
+set -U fish_color_quote a8a8a8
+set -U fish_color_redirection 808080
+set -U fish_color_end 949494
+set -U fish_color_error 585858
+set -U fish_color_param d7d7d7
+set -U fish_color_comment bcbcbc
+set -U fish_color_match --background=brblue
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_history_current --bold
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 777777
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel --reverse
+set -U fish_pager_color_prefix normal --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D
+set -U fish_pager_color_selected_background --background=brblack
+set -U fish_pager_color_selected_prefix
+set -U fish_color_host_remote
+set -U fish_color_keyword
+set -U fish_pager_color_secondary_completion
+set -U fish_pager_color_selected_completion
+set -U fish_pager_color_secondary_description
+set -U fish_color_option
+set -U fish_pager_color_secondary_background
+set -U fish_pager_color_background
+set -U fish_pager_color_selected_description
+set -U fish_pager_color_secondary_prefix
