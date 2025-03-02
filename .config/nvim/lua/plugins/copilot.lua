613acde8ee461018dd59873b4981afa4945eb97b
Nikita Galaiko <nikita@galaiko.rocks>
2025-03-02 10:32:27 +0100

    nvim: add copilot

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,36 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			panel = {
+				enabled = false,
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				debounce = 75,
+				keymap = {
+					accept = "<C-A>",
+					accept_word = false,
+					accept_line = false,
+					next = false,
+					prev = false,
+					dismiss = false,
+				},
+			},
+			filetypes = {
+				yaml = false,
+				markdown = false,
+				help = false,
+				gitcommit = false,
+				gitrebase = false,
+				hgcommit = false,
+				svn = false,
+				cvs = false,
+				["."] = false,
+			},
+		})
+	end,
+}
