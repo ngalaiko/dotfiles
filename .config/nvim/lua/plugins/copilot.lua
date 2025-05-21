c61c1f6a13e91d15189b01278aa678aedd9a2d90
Nikita Galaiko <nikita@galaiko.rocks>
2025-05-21 11:14:16 +0200

    nvim: use copilot.vim

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,36 +1,12 @@
 return {
-	"zbirenbaum/copilot.lua",
+	"https://github.com/github/copilot.vim",
 	cmd = "Copilot",
 	event = "InsertEnter",
 	config = function()
-		require("copilot").setup({
-			panel = {
-				enabled = false,
-			},
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				debounce = 75,
-				keymap = {
-					accept = "<C-A>",
-					accept_word = false,
-					accept_line = false,
-					next = false,
-					prev = false,
-					dismiss = false,
-				},
-			},
-			filetypes = {
-				yaml = false,
-				markdown = false,
-				help = false,
-				gitcommit = false,
-				gitrebase = false,
-				hgcommit = false,
-				svn = false,
-				cvs = false,
-				["."] = false,
-			},
+		vim.keymap.set("i", "<C-A>", 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false,
 		})
+		vim.g.copilot_no_tab_map = true
 	end,
 }
