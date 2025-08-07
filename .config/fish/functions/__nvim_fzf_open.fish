5ccc3e751944dc17f71c6a642d1ce10a6b375515
Nikita Galaiko <nikita@galaiko.rocks>
2025-08-07 09:05:42 +0200

    fish: smarter fzf open

--- .config/fish/functions/__nvim_fzf_open.fish
+++ .config/fish/functions/__nvim_fzf_open.fish
@@ -1,6 +1,21 @@
 function __nvim_fzf_open
-    set file (rg --files | fzf)
-    if test -n "$file"
-        nvim "$file"
+    # Check if we're in a git repo
+    if git rev-parse --git-dir >/dev/null 2>&1
+        # In git repo: show tracked files + directories
+        set selection (begin
+            git ls-files
+            git ls-tree -d -r --name-only HEAD
+        end | sort -u | fzf)
+    else
+        # Not in git repo: show all files and directories (including hidden)
+        set selection (find . -type f -o -type d | sed 's|^\./||' | grep -v '^\.$' | fzf)
+    end
+    
+    if test -n "$selection"
+        if test -d "$selection"
+            cd "$selection"
+        else
+            nvim "$selection"
+        end
     end
 end
