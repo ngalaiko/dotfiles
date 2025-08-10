667122ece2439bbcf7001e78d075d06fd6e3f1e3
Nikita Galaiko <nikita@galaiko.rocks>
2025-08-10 21:51:53 +0200

    nvim: more clenup

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -12,32 +12,5 @@ return {
 		local lint = require("lint")
 
 		lint.linters_by_ft = linters_by_ft
-
-		function debounce(ms, fn)
-			local timer = vim.uv.new_timer()
-			return function(...)
-				local argv = { ... }
-				timer:start(ms, 0, function()
-					timer:stop()
-					vim.schedule_wrap(fn)(unpack(argv))
-				end)
-			end
-		end
-
-		vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
-			group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-			callback = debounce(100, function()
-				local opts = {}
-
-				-- support monorepos by using cwd as detected by active lsp
-				local get_clients = vim.lsp.get_clients or vim.lsp.get_active_clients
-				local client = get_clients({ bufnr = 0 })[1]
-				if client then
-					opts.cwd = client.root_dir
-				end
-
-				lint.try_lint(nil, opts)
-			end),
-		})
 	end,
 }
--- .config/nvim/lua/plugins/packer.lua
+++
@@ -1,1 +0,0 @@
-return { "https://github.com/wbthomason/packer.nvim" }
