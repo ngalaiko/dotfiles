00567bde33bd9b02151f8ba93ff97beffb30d757
Nikita Galaiko <nikita@galaiko.rocks>
2026-04-29 08:23:33 +0200

    nvim: yamlls configure

--- .config/nvim/lsp/yamlls.lua
+++ .config/nvim/lsp/yamlls.lua
@@ -4,8 +4,13 @@ return {
 	root_markers = { ".git" },
 	settings = {
 		yaml = {
+			schemaStore = {
+				enable = true,
+				url = "https://www.schemastore.org/api/json/catalog.json",
+			},
 			schemas = {
-				["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/refs/tags/3.1.0/schemas/v3.0/schema.yaml"] = "/*",
+				["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/refs/tags/3.1.0/schemas/v3.0/schema.yaml"] = { "openapi.yaml", "openapi.yml", "**/openapi/*.yaml" },
+				["https://json-schema.org/draft/2020-12/schema"] = { "*.schema.yaml", "*.schema.json" },
 			},
 		},
 	},
